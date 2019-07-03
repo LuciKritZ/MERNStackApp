4d58bf667bb5b32545fe6489ca7ce06db9bd9582
Krishal Shah <khshah@cygnet.com>
2019-07-03 17:38:40 +0530

    #4 Styling the frontend with ReactStrap.

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavbar from './components/AppNavbar'
+import ShoppingList from '../src/components/ShoppingList'
 
 class App extends React.Component {
   render(){
     return (
       <div className="App">
         <AppNavbar />
+        <ShoppingList />
       </div>
     );
   }
