ae87f02098ebba5f28a5114c5bf0e94748fc4068
Krishal Shah <khshah@cygnet.com>
2019-07-03 16:32:51 +0530

    #3 Runing server and client together and Navbar.

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AppNavbar from './components/AppNavbar'
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <AppNavbar />
+      </div>
+    );
+  }
+}
+
+export default App;
