c7d41642fca6167b7c08f56c8306c4fd97952a77
Krishal Shah <khshah@cygnet.com>
2019-07-04 12:12:24 +0530

    #5 Redux for static data

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavbar from './components/AppNavbar'
 import ShoppingList from '../src/components/ShoppingList'
+import ItemModal from './components/itemModal';
+import {Container} from 'reactstrap';
+
+// Redux
+import {Provider} from 'react-redux';
+import store from './components/store';
 
 class App extends React.Component {
   render(){
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
     );
   }
 }
